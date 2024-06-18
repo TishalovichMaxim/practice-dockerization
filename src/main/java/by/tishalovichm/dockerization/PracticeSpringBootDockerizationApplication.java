6350e299e7be9fecb30ef2cbf013e7c2c47225e0
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-18 10:32:38 +0300

    Add basic spring boot app

---
+++ src/main/java/by/tishalovichm/dockerization/PracticeSpringBootDockerizationApplication.java
@@ -0,0 +1,13 @@
+package by.tishalovichm.dockerization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootDockerizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBootDockerizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/tishalovichm/dockerization/PracticeSpringBootDockerizationApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tishalovichm.dockerization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootDockerizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
